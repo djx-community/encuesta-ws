1727f57481776b656c00b19f66a81e6d929ee0cc
MIDLAJ C <mail.midlajc@gmail.com>
2022-10-16 19:19:12 +0530

    db connection and Schema Update

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const socketIo = require('socket.io')
 require('dotenv').config()
 const http = require('http')
+const db = require('./config/connection')
 
 const server = http.createServer()
 
 //interfaces
 const quickPlayInterface = require('./interface/quickPlayInterface')
 const hostInterface = require('./interface/hostInterface')
-const db = require('./config/connection')
 
 const io = new socketIo.Server(server, {
     cors: { origin: "*" }
